8cd930344349b8cb34ee8b454dc7a550654660bd
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-15 09:27:47 -0400

    add contact section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
   $(".arrow").click(function() {
     $('html, body').animate({
-        scrollTop: $(".work").offset().top
+        scrollTop: $(".knowledge").offset().top
     }, 1000);
+});
+$(".about_link").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".knowledge").offset().top
+  }, 1000);
+});
+$(".work_link").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".work").offset().top
+  }, 1000);
+});
+$(".contact_link").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".contact").offset().top
+  }, 1000);
 });
 })
