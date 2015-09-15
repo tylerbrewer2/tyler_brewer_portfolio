e91b4b6b8144db18c90004c8270f651670a5e19b
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-15 13:30:31 -0400

    add arrows and show links

--- js/script.js
+++ js/script.js
@@ -3,20 +3,32 @@ $(document).ready(function() {
     $('html, body').animate({
         scrollTop: $(".knowledge").offset().top
     }, 1000);
-});
-$(".about_link").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".knowledge").offset().top
-  }, 1000);
-});
-$(".work_link").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".work").offset().top
-  }, 1000);
-});
-$(".contact_link").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".contact").offset().top
-  }, 1000);
-});
+  });
+  $(".about_link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".knowledge").offset().top
+    }, 1000);
+  });
+  $(".work_link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".work").offset().top
+    }, 1000);
+  });
+  $(".contact_link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top
+    }, 1000);
+  });
+  $("#all_link").click(function(){
+    $(".design").fadeIn("slow");
+    $(".development").fadeIn("slow");
+  })
+  $("#design_link").click(function(){
+    $(".design").fadeIn("slow");
+    $(".development").fadeOut("slow");
+  });
+  $("#development_link").click(function(){
+    $(".development").fadeIn("slow");
+    $(".design").fadeOut("slow");
+  })
 })
