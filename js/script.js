bbb4f2783e94f5764cec1b575e3fe89792e70a23
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-09-15 17:37:56 -0400

    fix scroll location

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   $(".arrow").click(function() {
     $('html, body').animate({
-        scrollTop: $(".knowledge").offset().top
+        scrollTop: $(".scroll-knowledge").offset().top
     }, 1000);
   });
   $(".about_link").click(function() {
     $('html, body').animate({
-        scrollTop: $(".knowledge").offset().top
-    }, 1000);
+        scrollTop: $(".scroll-knowledge").offset().top
+    }, "1000");
   });
   $(".work_link").click(function() {
     $('html, body').animate({
-        scrollTop: $(".work").offset().top
+        scrollTop: $(".scroll-work").offset().top
     }, 1000);
   });
   $(".contact_link").click(function() {
