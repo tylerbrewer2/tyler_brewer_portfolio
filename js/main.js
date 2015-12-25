4f4c59e492acfd3aea88033cf01202b93dad1a99
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-12-25 12:37:53 -0500

    add header React component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
-alert("Hello! How are you doing?");
+/*
+  HeaderNav
+  <HeaderNav />
+*/
+
+var HeaderNav = React.createClass({
+  render : function() {
+    return (
+      <div className="content">
+        <ul>
+          <li className="nav-links"><a href="../images/tyler_brewer_resume.pdf" target="_blank">Resumé</a></li>
+          <li className="nav-links"><a href="https://medium.com/@tyler_brewer2" target="_blank">Blog</a></li>
+          <li className="nav-links"><a href="../index.html">Home</a></li>
+        </ul>
+        <a href="../index.html"><img className="logo" src="../images/logo.png"></img></a>
+        <div className="info">
+          <h2 className="logo_name">TYLER BREWER</h2>
+          <p className="logo_description"><em>web developer and designer</em></p>
+        </div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<HeaderNav/>, document.querySelector(".navigation"));
